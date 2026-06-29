6546ccb3c3e4139c9af7306527d6e56fa49535fb
hanasbp <hanasoke@gmail.com>
2026-06-29 20:09:46 +0700

    updated main.go & user.go

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct di atas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	c.JSON(http.StatusOK, nil)
+
+}
