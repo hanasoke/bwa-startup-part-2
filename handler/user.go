2222aaabc0ed817ea8d5de46712b5b4365d85673
hanasbp <hanasoke@gmail.com>
2026-07-01 11:51:56 +0700

    updated formatter.go & user.go

--- handler/user.go
+++ handler/user.go
@@ -28,13 +28,17 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+	// token, err := h.jwtService.GenerateToken()
+
+	formatter := user.FormatUser(newUser, "tokentokentokentoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
 
