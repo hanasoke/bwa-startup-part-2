b871baf2d02fc00c4c5d43e4a140680474d5670e
hanasbp <hanasoke@gmail.com>
2026-07-07 11:03:41 +0700

    complete handler api login endpoint in project

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bwastartup/handler"
 	"bwastartup/user"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -22,33 +21,14 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
-	input := user.LoginInput{
-		Email:    "com.agungsetiawan@gmail.com",
-		Password: "password",
-	}
-
-	user, err := userService.Login(input)
-	if err != nil {
-		fmt.Println("Terjadi kesalahan")
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
-
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run()
-
-	// Input dari user
-	// handler, input dari user -> struct input
-	// service : melakukan mapping dari struct input ke struct User
-	// repository
-	// db
 
 }
