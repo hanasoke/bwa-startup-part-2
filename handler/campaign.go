fd063fe9345cf78a7697d143842654aa77579296
hanasbp <hanasoke@gmail.com>
2026-07-17 14:28:41 +0700

    added repository for Campaign API list

--- handler/campaign.go
+++ handler/campaign.go
@@ -3,5 +3,5 @@ package handler
 // tangkap parameter di handler
 // handler ke service
 // service yang menentukan repository mana yang di-call
-// repository : GetAll, GetByUserID
+// repository : FindAll, FindByUserID
 // db
