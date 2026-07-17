7abad70cff02e30fdbf3e4fbd3e09212f6325f12
hanasbp <hanasoke@gmail.com>
2026-07-17 16:39:40 +0700

    Campaign repository

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, err := campaignRepository.FindByUserID(2)
+	campaigns, err := campaignRepository.FindByUserID(1)
 
 	fmt.Println("debug")
 	fmt.Println("debug")
@@ -36,6 +36,11 @@ func main() {
 	fmt.Println(len(campaigns))
 	for _, campaign := range campaigns {
 		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println("jumlah gambar")
+			fmt.Println(len(campaign.CampaignImages))
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
 	}
 
 	userService := user.NewService(userRepository)
