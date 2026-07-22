6383d074da9d9992d0d7b5d27c5b928a4dd8e25c
hanasbp <hanasoke@gmail.com>
2026-07-22 16:40:38 +0700

    updated formatter.go & campaign.go

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -26,3 +26,14 @@ func FormatCampaign(campaign Campaign) CampaignFormatter {
 
 	return campaignFormatter
 }
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	var campaignsFormatter []CampaignFormatter
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
--- handler/campaign.go
+++ handler/campaign.go
@@ -22,7 +22,7 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 
 	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
