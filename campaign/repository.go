466045be9162481806eab7dabf8525b2a0cbdeba
hanasbp <hanasoke@gmail.com>
2026-07-27 10:49:26 +0700

    updated repository.go

--- campaign/repository.go
+++ campaign/repository.go
@@ -41,7 +41,7 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
 func (r *repository) FindByID(ID int) (Campaign, error) {
 	var campaign Campaign
 
-	err := r.db.Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
+	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
 
 	if err != nil {
 		return campaign, err
