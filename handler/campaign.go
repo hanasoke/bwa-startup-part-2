3a09fd0b64856e3a43d30466a7b1586b759652f7
hanasbp <hanasoke@gmail.com>
2026-07-27 10:47:03 +0700

    updated 3 golang pages

--- handler/campaign.go
+++ handler/campaign.go
@@ -30,3 +30,9 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	// handler : mapping id yg di url ke struct input => service, call formatter
+	// service : inputnya struct input => menangkap id di url, manggil repo
+	// repository : get campaign by id
+}
