d692e6f65c3db72f2b50f25a26264adc93db16a8
hanasbp <hanasoke@gmail.com>
2026-07-28 12:57:07 +0700

    updated formatter.go & campaign.go

--- handler/campaign.go
+++ handler/campaign.go
@@ -32,11 +32,6 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 }
 
 func (h *campaignHandler) GetCampaign(c *gin.Context) {
-	// api/v1/campaigns/2
-	// handler : mapping id yg di url ke struct input => service, call formatter
-	// service : inputnya struct input => menangkap id di url, manggil repo
-	// repository : get campaign by id
-
 	var input campaign.GetCampaignDetailInput
 
 	err := c.ShouldBindUri(&input)
@@ -46,13 +41,13 @@ func (h *campaignHandler) GetCampaign(c *gin.Context) {
 		return
 	}
 
-	campaign, err := h.service.GetCampaignByID(input)
+	campaignDetail, err := h.service.GetCampaignByID(input)
 	if err != nil {
 		response := helper.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaign)
+	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
 	c.JSON(http.StatusOK, response)
 }
