3d084e465c38e12822faba0c447c94d4873cf719
hanasbp <hanasoke@gmail.com>
2026-08-04 23:58:30 +0700

    updated main.go

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
 	api.GET("/campaigns/:id", campaignHandler.GetCampaign)
+	api.POST("/campaigns", authMiddleware(authService, userService), campaignHandler.GetCampaign)
 
 	router.Run()
 }
